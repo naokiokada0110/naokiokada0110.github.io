0526f0260fb48f4d0af36970553c7f70bdf7c2b8
naokiokada0110 <okdman.agi@gmail.com>
2020-09-19 11:32:24 +0900

    JS設定

---
+++ index.js
@@ -0,0 +1,62 @@
+$(function(){
+  ScrollReveal().reveal('h1',{
+    duration:1500,
+    viewFactor: 0.2, 
+  });
+
+  ScrollReveal().reveal('.twitter',{
+    duration:2500,
+    viewFactor: 0.2, 
+  });
+
+  ScrollReveal().reveal('.instagram',{
+    duration:3000,
+    viewFactor: 0.2, 
+  });
+
+  ScrollReveal().reveal('.img_face',{
+    duration:2000,
+    viewFactor: 0.2, 
+  });
+
+  ScrollReveal().reveal('dt',{
+    duration:2500,
+    viewFactor: 0.2, 
+  });
+
+  ScrollReveal().reveal('dd',{
+    duration:2500,
+    viewFactor: 0.2, 
+  });
+
+  ScrollReveal().reveal('.textbox',{
+    duration:2500,
+    viewFactor: 0.2, 
+  });
+
+  ScrollReveal().reveal('.photo',{
+    duration:2000,
+    viewFactor: 0.2, 
+  });
+
+  ScrollReveal().reveal('.text',{
+    duration:2500,
+    viewFactor: 0.2, 
+  });
+
+  ScrollReveal().reveal('button',{
+    duration:2500,
+    viewFactor: 0.2, 
+  });
+  
+  ScrollReveal().reveal('.contact_text',{
+    duration:2000,
+    viewFactor: 0.2, 
+  });
+  
+  ScrollReveal().reveal('iframe',{
+    duration:2500,
+    viewFactor: 0.2, 
+  });
+  
+});
